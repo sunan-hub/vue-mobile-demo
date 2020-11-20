c5ddc5aa3e693f29b6182c8a7362ebb68bc8d17b
jiren.li <jiren.li@digitalhainan.com.cn>
2020-11-20 16:49:17 +0800

    refactor: 移除单独设置dev环境baseUrl

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import HttpRequest from './axios'
 import config from '@/config'
-const baseUrl = process.env.NODE_ENV === 'development' ? '/api/' : config.baseUrl
+const baseUrl = config.baseUrl
 
 const axios = new HttpRequest(baseUrl)
 
